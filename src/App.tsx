49896926a3b0901fd94246998e505ee51ce44c7f
Gabriel Crispino <gabriel.crispino@hotmail.com>
2019-10-28 13:14:04 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {textToMorse, morseToGap} from './Utils/textToMorse';
+
+// create web audio api context
+var audioCtx = new(window.AudioContext)();
+
+const sleep = (ms: number) => 
+  new Promise(resolve => setTimeout(resolve,ms));
+
+function playNote(frequency: number, duration: number) {
+  // create Oscillator node
+  var oscillator = audioCtx.createOscillator();
+  
+  oscillator.type = 'square';
+  oscillator.frequency.value = frequency; // value in hertz
+  oscillator.connect(audioCtx.destination);
+  return async () => {
+    oscillator.start();
+    await sleep(duration);
+  oscillator.stop();
+}
+}
+
+const seq = ([p, ...promises]: (() => Promise<void>)[]): Promise<void> => {
+  return Array.isArray(promises) && promises.length !== 0
+    ? p().then(() => seq(promises))
+    : p().then()
+}
+
+const playNotes = (notes: {
+  gap: number,
+  hasSound: boolean
+}[]) => {
+  const defaultGap = 100;
+  const defaultFrequency = 150;
+
+  const ps = notes.map(({ gap, hasSound }) => {
+    return playNote(
+      hasSound ? defaultFrequency : 0, defaultGap * gap);
+  });
+
+  return seq(ps);
+}
 
 const App: React.FC = () => {
+  const [text, setText] = React.useState('')
+  const [morse, setMorse] = React.useState('');
+  const [hasSound, setSound] = React.useState(false);
+
+  React.useEffect(() => {
+    if (morse.length !== 0 && hasSound)
+      playNotes(morseToGap(morse)).then(() => setSound(false));
+  }, [morse, hasSound])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="checkbox" checked={hasSound} onChange={() => setSound(!hasSound)} />
+      <input value={text} onChange={(e)=> {
+        setMorse(textToMorse(e.target.value))
+        setText(e.target.value)
+        }} />
+      <p>{morse}</p>
     </div>
   );
 }
---
+++ src/Utils/textToMorse.ts
@@ -0,0 +1,93 @@
+const text2morseH = {
+    'A': ".-",
+    'B': "-...",
+    'C': "-.-.",
+    'D': "-..",
+    'E': ".",
+    'F': "..-.",
+    'G': "--.",
+    'H': "....",
+    'I': "..",
+    'J': ".---",
+    'K': "-.-",
+    'L': ".-..",
+    'M': "--",
+    'N': "-.",
+    'O': "---",
+    'P': ".--.",
+    'Q': "--.-",
+    'R': ".-.",
+    'S': "...",
+    'T': "-",
+    'U': "..-",
+    'V': "...-",
+    'W': ".--",
+    'X': "-..-",
+    'Y': "-.--",
+    'Z': "--..",
+    '1': ".----",
+    '2': "..---",
+    '3': "...--",
+    '4': "....-",
+    '5': ".....",
+    '6': "-....",
+    '7': "--...",
+    '8': "---..",
+    '9': "----.",
+    '0': "-----",
+    '.': ".-.-.-",
+    ',': "--..--",
+    ':': "---...",
+    '?': "..--..",
+    '\'': ".----.",
+    '-': "-....-",
+    '/': "-..-.",
+    '(': "-.--.",
+    ')': "-.--.-",
+    '"': ".-..-.",
+    '@': ".--.-.",
+    '=': "-...-",
+    '&': ".-...",
+    '+': ".-.-.",
+    '!': "-.-.--",
+    ' ': "/"
+} as {
+    [key:string]: string;
+}
+
+export const textToMorse = (word: string) => 
+    word
+        .trim()
+        .replace(/\s+/g, ' ')
+        .split('')
+        .map(c => text2morseH[c.toUpperCase()])
+        .join(' ');
+
+// short mark, dot or "dit" (▄▄▄▄): 1
+// longer mark, dash or "dah" (▄▄▄▄▄▄): 111
+// intra-character gap (between the dots and dashes within a character): 0
+// short gap (between letters): 000
+// medium gap (between words): 000 0 000 
+const marks = {
+    '-': {gap: 3, hasSound: true },
+    '.': {gap: 1, hasSound: true },
+    ' ': {gap: 3, hasSound: false },
+    '/': {gap: 1, hasSound: false },
+    '*': {gap: 1, hasSound: false}
+} as {
+    [key:string]: {
+        gap: number,
+        hasSound: boolean
+    };
+}
+
+export const morseToGap = (morseCode: string) => 
+    morseCode
+        .replace(/--/g,'-*-') 
+        .replace(/-\./g,'-*.') 
+        .replace(/\.-/g,'.*-')
+        .replace(/\.\./g,'.*.')
+        .split('')
+        .map(char => marks[char]);
+
+export default textToMorse;
